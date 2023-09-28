4f9ba01165a9a66184ada3ba9af53cf147a91133
polezhaevr <polezhaevr89@gmail.com>
2023-09-28 18:47:17 +0500

    added new project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header(props) {
+  return (
+    <header className="header">
+        <h1>Hello World</h1>
+    </header>
+  );
+}
+
+export default Header;
